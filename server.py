422db05d914940ea83e26323e2162c03df1de821
Paul Lam <109431478+Pauullamm@users.noreply.github.com>
2024-06-19 14:41:42 +0000

    cross origin adjustment

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 socketio = SocketIO(app)
 
 # Store chat messages
